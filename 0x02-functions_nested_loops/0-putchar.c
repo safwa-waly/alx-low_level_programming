6b105d389110c9d41cf4b24df9d2f3f15a5bfac7
safwa <ss@gmail.com>
2023-03-21 13:28:07 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char s[] = "_putchar\n";
+int i = 0;
+while (s[i])
+{
+_putchar(s[i]);
+i++;
+}
+return (0);
+}
